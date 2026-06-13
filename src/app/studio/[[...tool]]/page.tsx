9a8a2046f96d2f9fa5a97448c49dd2cccb36a7e8
InshaKhan6593 <ik0084204@gmail.com>
2026-06-13 19:25:59 +0500

    fix embedded Sanity Studio mount

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import { NextStudio } from "next-sanity/studio";
 
 import config from "../../../../sanity.config";
-
-const NextStudio = dynamic(
-  () => import("next-sanity/studio").then((mod) => mod.NextStudio),
-  { ssr: false },
-);
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
